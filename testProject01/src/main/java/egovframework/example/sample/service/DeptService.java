29d58a76ec5974ffd501f74d2cefa1e4f85a2b10
matodale <sowo10@naver.com>
2023-01-05 22:35:17 +0900

    20230105_firstUpload

---
+++ testProject01/src/main/java/egovframework/example/sample/service/DeptService.java
@@ -0,0 +1,11 @@
+package egovframework.example.sample.service;
+
+public interface DeptService {
+	/*
+	 * DeptService.java 
+	 * interface 선언
+	 * */
+	public String InsertDept(DeptVo vo) throws Exception;
+	
+}
+
